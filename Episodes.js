d946a5df6b978c77a1f6c3defe6f860b21717216
MarkDrastrup <mark.drastrup@gmail.com>
2019-08-07 08:00:33 +0900

    First commit

---
+++ Episodes.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Button, TouchableOpacity, ScrollView } from 'react-native';
+import { Audio } from 'expo-av';
